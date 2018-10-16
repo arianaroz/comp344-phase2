7784b0c2fe60dec52052c0a810d191dbdd7d298d
arianaroz <36976168+arianaroz@users.noreply.github.com>
2018-10-16 20:47:36 +1100

    Create login.php

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+
+include("common_db.php");
+
+?>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Login</title>
+<link rel="stylesheet" href="style.css" >
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+</head>
+
+<script type="text/javascript">
+
+
+</script>
+
+
+
+<div class="loginContainer">
+
+<form class="signin" method="POST">
+      <h2 class="signin-heading">Sign In</h2>
+      <div class="error"><?php echo $error;?></div>
+      <div class="input-group">
+    <input type="text" name="email" class="login-form" placeholder="Email" required>
+  </div>
+  <div>
+      <input type="password" name="password" id="inputPassword" class="login-form" placeholder="Password" required>
+     <div> <button id="login-btn" class="button" type="submit" name="login">Sign In</button> </div>
+     <div id="link">Don't have an account? <a id="registerlink" href="register.html">Sign Up</a></div>
+     </div>
+    </form>
+
+    
+</div>
+
+</body>
+</html>
