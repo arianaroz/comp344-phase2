1ea4142d9f4251c6db45880ed4bf0368c770192e
Ariana <ariana.rozsnyoi@gmail.com>
2018-10-19 14:41:29 +1100

    Footer changes

--- footer.php
+++ footer.php
@@ -6,9 +6,37 @@
 <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 </head>
 
-
+<div id="main">
 <footer class= "footer">
 
-<menu> About</menu>
+<div class="col">
+<div class="colleft">
+    <menu> <h4>ABOUT</h4></menu>
+        <div class="ff">
+            <menu> About Us</menu>
+            <menu> Contact </menu>
+            <menu> Privacy Policy</menu>
+            <menu> Terms & Conditions</menu>
+        </div>
+</div>
+
+<div class="colcenter">
+    <menu><h4> SUPPORT </h4></menu>
+    <div class="ff">
+        <menu> FAQs</menu>
+        <menu> Shipping </menu>
+        <menu> Payment </menu>
+    </div>
+</div>
+</div>
+
+
+
+
+<div id="copyright">
+<a> &copy; Super Notebook Store</a>
+</div>
 </footer>
+
+</div>
 </html>
