785de32e049da7ad7c2b398141e68200bbb087bc
Ariana <ariana.rozsnyoi@gmail.com>
2018-10-21 21:39:14 +1100

    signin

--- signin.php
+++ signin.php
@@ -19,32 +19,60 @@ include("db.php");
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Login</title>
-<link rel="stylesheet" href="style1.css" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+
 </head>
-<body>
+
 <script type="text/javascript">
-
 </script>
-<section class="hero has-background-white-bis is-medium">
-<div class="hero-body">
-        <div class="loginContainer">
 
-        <form class="signin" method="POST">
-            <h5 class="title is-5">Sign In</h5>
+<body>
+<section class="hero has-background-white-bis is-medium">
+    <div class="hero-body">
+        <div class="logincontainer has-background-white">
+            <h5 class="title is-5">Sign in</h5>
             <div class="error"><?php echo $error;?></div>
-            <input type="text" name="username" class="login-form" placeholder="Username" required>
-        <div>
-            <input type="password" name="password" id="inputPassword" class="login-form" placeholder="Password" required>
-            <div> <button id="login-btn" class="button" type="submit" name="login">Sign In</button> </div>
-            <div id="passlink"> <a id="passwordlink" href=""> Forgot your password? </a> </div>
-            <div id="link">Don't have an account? <a id="registerlink" href="register.php">Sign Up</a></div>
-        </div>
-    </form>
+         <div class="columns is-centered">
+              <div class="card-content">
+
+            <div class="field ">
+  <p class="control has-icons-left ">
+    <input class="input" type="text" name="username" placeholder="Username" required>
+    <span class="icon is-small is-left">
+      <i class="fas fa-user"></i>
+  </span>
+  </p>
+</div>
+<div class="field">
+  <p class="control has-icons-left">
+    <input class="input" type="password" name="password" placeholder="Password" required>
+    <span class="icon is-small is-left">
+      <i class="fas fa-lock"></i>
+    </span>
+  </p>
+</div>
+<div class="field">
+  <p class="control">
+    <button class="button is-fullwidth has-background-primary" type="submit" name="login">
+      Login
+    </button>
+  </p>
+</div>
+
+<div class="field ">
+    <div class="is-size-7" id="passlink"> <a id="passwordlink" href="forgotPassword.php"> Forgot your password? </a> </div>
+    <div class="is-size-7" id="link">Don't have an account? <a id="registerlink" href="register.php">Sign Up</a></div>
 </div>
 </div>
-</section>
+</div>
+</div>
+
+
+</div>
+</div>
+</section
+
 <div class="footer">
 <?php include("footer.php"); ?>
 </div>
